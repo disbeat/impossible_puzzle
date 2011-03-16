9d0e631754c181024679d95a4cd0a8607d61bb1a
Marco Simoes <disbeat@gmail.com>
2011-03-16 17:23:49 +0000

    piece generalized for any side size

--- piece.py
+++ piece.py
@@ -9,49 +9,57 @@ DOWN_RIGHT = 2
 UP_RIGHT = 3
 
 class Piece( object ):
+	''' Class that represents a puzzle piece. It has its contents in self.parts. One can request sides or corners using the above directions. '''
 	n_sides = 4
+	side_size = 2
 	
 	def __init__(self, parts, pid):
+		''' Piece initialization. Set the content (parts) of the piece, its id and the base orientation. '''
 		self.parts = parts
 		self.orientation = 0
 		self.pid = pid
 	
 	def __str__(self):
+		''' String representation of the piece. '''
 		return str(self.pid)
 	
 	def __eq__(self, other):
+		''' Comparison needed for copied objects matches. '''
 		return self.pid == other.pid
 		
 	def __repr__(self):
+		''' Representation needed for list prints. '''
 		return str(self.pid)
 		
 	def sides(self):
+		''' Return a list of the sides as tuples. '''
 		return [ self.side(i) for i in range(self.n_sides) ]
 		
-	def side(self, i):
-		return ( self.parts[ (self.orientation + i) % self.n_sides ], self.parts[ (self.orientation + i + 1) % self.n_sides ] )
+	def side(self, direction):
+		''' Returns a side tuple correspondent to the given direction. '''
+		return tuple([ self.parts[ (self.orientation + direction * (self.side_size-1) + i) % self.n_sides ] for i in range(self.side_size) ])
 		
 	def corners(self):
+		''' Returns a list of the corners as tuples. '''
 		return [ self.corner(i) for i in range(self.n_sides) ]
 	
-	def corner(self, i):
-		return ( self.parts[ (self.orientation + i - 1) % self.n_sides ], \
-				 self.parts[ (self.orientation + i) % self.n_sides ], 	 \
-				 self.parts[ (self.orientation + i + 1) % self.n_sides ] )
+	def corner(self, direction):
+		''' Returns a corner as tuple. The corner corresponds to the both sides it joins united. '''
+		return tuple([ self.parts[ (self.orientation + direction * (self.side_size-1) + i ) % self.n_sides ] for i in range(-(self.side_size-1), self.side_size) ])
 		
 	def rotate(self, times = 1):
-		self.orientation = ( self.orientation + times ) % self.n_sides
+		''' Rotates the piece 90 degrees counter clockwise. '''
+		self.orientation = ( self.orientation + times * (self.side_size-1) ) % self.n_sides
 		
-	def positionate_side(self, target_side, target_direction):
-		self.orientation = (self.parts.index(target_side[0]) - target_direction ) % self.n_sides
+	def positionate_side(self, side, direction):
+		''' Orientates the piece so side places at direction '''
+		self.orientation = (self.parts.index(side[0]) - direction * (self.side_size-1) ) % self.n_sides
 	
-	def positionate_corner(self, target_corner, target_direction):
-		self.orientation = (self.parts.index(target_corner[1]) - target_direction ) % self.n_sides
+	def positionate_corner(self, corner, direction):
+		''' Orientates the piece so corner places at direction '''
+		self.orientation = (self.parts.index(corner[1]) - direction * (self.side_size-1) ) % self.n_sides
 		
-	def match_sides(self, self_side, target_side):
-		return self_side[1] == target_side[0] and self_side[0] == target_side[1]
 	
-
 def test():
 
 	p = Piece( [0, 1, 2, 3], 0 )
